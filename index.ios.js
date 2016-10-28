947078d25ffa91346147d30e2ee4ed675741fa0b
Colin J Riddell <colinjohnriddell@gmail.com>
2016-10-28 15:50:43 +0100

    Add stop and lap functionality. Add lap styling. Finished basic application. MVP

--- index.ios.js
+++ index.ios.js
@@ -16,7 +16,10 @@ var StopWatch = React.createClass({
   getInitialState: function() { // Ran by React exactly one time, when the component is first created
     // Returns an object, and always avoid putting logic into getInitialState()
     return {
-      timeElapsed: null
+      timeElapsed: null,
+      running: false,
+      startTime: null,
+      laps: []
     }
   },
   render: function(){
@@ -34,37 +37,67 @@ var StopWatch = React.createClass({
       </View>
 
       <View style={[styles.footer]}>
-        <Text>
-          I am  a list of laps
-        </Text>
+        {this.laps()}
       </View>
     </View>
   },
+  laps: function() {
+    return this.state.laps.map(function(time, index){
+      return <View style={styles.lap}>
+        <Text style={styles.lapText}>
+          Lap #{index + 1}
+        </Text>
+        <Text style={styles.lapText}>
+          {formatTime(time)}
+        </Text>
+      </View>
+    });
+  },
   startStopButton: function() {
+    var style = this.state.running ? styles.stopButton : styles.startButton;
     return <TouchableHighlight
       underlayColor="gray"
       onPress={this.handleStartPress.bind(this)}
-      style={[styles.button, styles.startButton]}>
+      style={[styles.button, style]}>
       <Text>
-      Start
+      {this.state.running ? 'Stop' : 'Start'}
       </Text>
     </TouchableHighlight>
   },
   lapButton: function() {
-    return <View style={styles.button}>
+    return <TouchableHighlight
+      style={styles.button}
+      underlayColor="gray"
+      onPress={this.handleLapPress}>
         <Text>
         Lap
         </Text>
-      </View>
+      </TouchableHighlight>
+  },
+  handleLapPress: function() {
+    // get the time elapsed
+    var lap = this.state.timeElapsed;
+
+    // reset the timer to zero
+    this.setState({
+      startTime: new Date(),
+      laps: this.state.laps.concat([lap]) // use concat instead of push because of state rules
+    });
   },
   handleStartPress: function(){
+    if (this.state.running) {
+      clearInterval(this.interval);
+      this.setState({running: false})
+      return
+    }
     //console.log('start was pressed');
-    var startTime = new Date();
+    this.setState({startTime: new Date()});
 
     // Updating state causes the screen to re-render
-    setInterval(() => { //anonymous fat arrow function call
+    this.interval = setInterval(() => { //anonymous fat arrow function call
       this.setState({  // Always call this.setState to update state. Give object
-        timeElapsed: new Date() - startTime
+        timeElapsed: new Date() - this.state.startTime,
+        running: true
       });
     }, 30);
   }
@@ -105,6 +138,17 @@ var styles = StyleSheet.create({
   },
   startButton: {
     borderColor: '#00CC00'
+  },
+  stopButton: {
+    borderColor: '#CC0000'
+  },
+  lap: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    backgroundColor: 'gray'
+  },
+  lapText : {
+    fontSize: 30
   }
 });
 
